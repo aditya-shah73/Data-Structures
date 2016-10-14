b2be8e16d8eb445df91d338c7676810a56baa42e
aditya7395 <aditya7395@gmail.com>
2016-10-13 22:57:53 -0700

    Hashmap questions

---
+++ src/Interview_Programs/ArrayPairSum.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class ArrayPairSum 
+{
+	public static void printSumPairs(int[] a, int k)
+	{
+		HashMap<Integer, Integer> h = new HashMap<Integer, Integer>();
+
+		for(int i=0;i<a.length;i++)
+		{
+			if(h.containsKey(a[i]))
+			{
+				System.out.println(a[i] +", "+ h.get(a[i]));
+			}
+			else
+			{
+				h.put(k-a[i], a[i]);
+			}
+		}
+	}
+	public static void main(String[] args) 
+	{        
+		int []a = {2,45,7,3,5,1,8,9};
+		printSumPairs(a,10);        
+	}
+}
---
+++ src/Interview_Programs/FirstUniqueCharacter.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class FirstUniqueCharacter
+{
+	public static void firstUnique(String s)
+	{
+		char c;
+		HashMap<Character,Integer> h = new HashMap<>();
+		for(int i=0;i<s.length();i++)
+		{
+			c = s.charAt(i);
+			if(h.containsKey(c))
+			{
+				h.put(c, h.get(c)+1);
+			}
+			else
+			{
+				h.put(c, 1);
+			}
+		}
+		for(int j=0;j<s.length();j++)
+		{
+			char d;
+			d = s.charAt(j);
+			if(h.get(d) == 1)
+			{
+				System.out.println(d);
+				break;
+			}
+		}
+	}
+	
+	public static void main(String args[])
+	{
+		String s="aditya";
+		firstUnique(s);
+	}
+}
